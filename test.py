c8100e0443e8785ee927ee96902151a1dca60ac3
Luccarjgh <luccacsg@gmail.com>
2023-08-19 15:36:15 -0300

    changed to message system testing

--- test.py
+++ test.py
@@ -1,29 +1,75 @@
 import socket
-import sys
 import pickle
-from network_tools import send_data, recv_packets, ImageData
+from network_tools import *
+import os
 
 HOST, PORT = socket.gethostbyname(socket.gethostname()), 5555
 
-filepath = ' '.join(sys.argv[1:])
-
-with open(filepath, 'rb') as fb:
-    data = ImageData(fb.read(), filepath.split('.')[-1])
+username = input('Username: ')
 
 # IPV4 & TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
-    conn.connect((HOST, PORT))
-    
-    send_data(conn, data)
-    
-    try:
-        received = recv_packets(conn)
-    except TimeoutError:
-        print('Server stopped.')
-        quit()
-    
-    received = pickle.loads(received)
-    if received:
-        print('The file was moved successfully')
-    else:
-        print('Something went wrong')
+	conn.connect((HOST, PORT))
+
+	# first thing the client should send is his username.
+	send_data(conn, username)
+
+	username = recv_data(conn)
+	if not username:
+		print('Server stopped?')
+		quit()
+	
+	username = pickle.loads(username)
+	
+	print('Commands:\n/file <path>')
+	while True:
+		try:
+			message = input(f'{username}: ')
+		except KeyboardInterrupt:
+			break
+		
+		if message.startswith('/'):
+			if len(message) == 1:
+				continue
+			
+			message = message.split(' ', 1)
+			if len(message) == 1:
+				# commands that does not require arguments goes here
+				command = message
+			
+			else: # commands that require arguments goes here
+			
+				command, arguments = message
+				command = command[1:]
+				
+				if command == 'file':
+					if not os.path.exists(arguments):
+						print('Path does not exists')
+						continue
+
+					file_extension = os.path.splitext(arguments)[-1][1:]
+					if not file_extension:
+						print('Missing file extension')
+						continue
+
+					with open(arguments, 'rb') as fb:
+						send_data(conn, FileData(fb.read(), file_extension, 'wb', 'rb'))
+
+		else:
+			data = MessageData(message, 'server')
+			send_data(conn, data)
+
+			result = recv_data(conn)
+
+			if check(result, ClientNotFoundError):
+				print(f'{data.receiver} not found')
+
+		try:
+			received = recv_data(conn)
+		except TimeoutError:
+			print('Server stopped')
+			quit()
+		
+		received = pickle.loads(received)
+		if not received:
+			print('Oops! Something went wrong...')
